d02d3513dd3406c8cf3b9c28ec26ff2b48e030ed
jmpurvis0 <107958250+jmpurvis0@users.noreply.github.com>
2023-09-10 14:27:45 -0400

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -1,15 +1,13 @@
 import pandas as pd
-pd.set_option("display.max_rows", 5)
-reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip", index_col = 0)
+pd.set_option("display.max_rows", 10)
+reviews = pd.read_csv("./data/winemag-data-130k-v2.csv.zip", index_col = 0)
 
 count = reviews.country.value_counts()
 avg_points = reviews.groupby('country')['points'].mean().round(1)
 reviews_merged = pd.DataFrame.merge(count, avg_points, on = 'country', how = 'inner')
+reviews_merged.to_csv('./data/reviews-per-country.csv')
 
 print(reviews_merged)
-
-with open('data/reviews-per-country.csv', 'w') as csv_file:
-    reviews_merged.to_csv(path_or_buf = csv_file)
 
 
 
